c8008911219684eee6c82063709813923420449f
yigitismail <smlygt@icloud.com>
2020-02-05 23:53:15 +0300

    Skor arttı.

--- app/src/main/java/com/yigitismail/catchthekenny/MainActivity.java
+++ app/src/main/java/com/yigitismail/catchthekenny/MainActivity.java
@@ -3,12 +3,34 @@ package com.yigitismail.catchthekenny;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timeText;
+    TextView scoreText;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize
+
+        timeText = findViewById(R.id.timeText);
+        scoreText = findViewById(R.id.scoreText);
+        score = 0;
+
+    }
+
+    public void increaseScore(View view){
+
+        score ++;
+
+        scoreText.setText("Score: " + score);
+
+
     }
 }
